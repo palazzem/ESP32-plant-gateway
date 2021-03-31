c37c5780f6588feeff3abce09a2aae10eddc6a65
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2021-03-31 13:42:28 +0200

    Add Plant struct testing

--- test/test_main.cpp
+++
@@ -1,9 +0,0 @@
-#include <unity.h>
-
-void test_generic(void) { TEST_ASSERT_EQUAL(1, 1); }
-
-int main(void) {
-  UNITY_BEGIN();
-  RUN_TEST(test_generic);
-  return UNITY_END();
-}
---
+++ test/test_plant.cpp
@@ -0,0 +1,19 @@
+#include <unity.h>
+
+#include "plant.h"
+
+void test_plant_struct(void) {
+  Plant plant{"AA:00:04:00:XX:YY"};
+  TEST_ASSERT_EQUAL_STRING("AA:00:04:00:XX:YY", plant.macAddr);
+  TEST_ASSERT_EQUAL(0, plant.metrics.temperature);
+  TEST_ASSERT_EQUAL(0, plant.metrics.moisture);
+  TEST_ASSERT_EQUAL(0, plant.metrics.light);
+  TEST_ASSERT_EQUAL(0, plant.metrics.conductivity);
+  TEST_ASSERT_EQUAL(0, plant.metrics.battery);
+}
+
+int main(void) {
+  UNITY_BEGIN();
+  RUN_TEST(test_plant_struct);
+  return UNITY_END();
+}
